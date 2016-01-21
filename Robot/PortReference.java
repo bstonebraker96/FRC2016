a966d62ad1adecd8c68a84df737db7f05d18a853
BeijingStrongbow <ericdhiggins@gmail.com>
2016-01-21 15:26:48 -0600

    Reference ports here

---
+++ Robot/PortReference.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team5968.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class PortReference {
+	
+	public int getLeftJoystick(){
+		return 1;
+	}
+	
+	public int getRightJoystick(){
+		return 2;
+	}
+	
+	public int getLeftMotor(){
+		return 1;
+	}
+	
+	public int getRightMotor(){
+		return 2;	
+	}
+	
+	public int getLeftEncoder1(){
+		return 3;
+	}
+	
+	public int getLeftEncoder2(){
+		return 4;
+	}
+	
+	public int getRightEncoder1(){
+		return 5;
+	}
+	
+	public int getRightEncoder2(){
+		return 6;
+	}
+	
+}
